5c7734bb2d0dcee990574de4478714256d8765c6
Kar205tik <mittal.kartik2005@gmail.com>
2026-07-15 02:51:05 +0530

    Update Business Problems Netflix.sql

--- Business Problems Netflix.sql
+++ Business Problems Netflix.sql
@@ -1,22 +1,81 @@
--- 15 Business Problems & Solutions
-
-1. Count the number of Movies vs TV Shows
-2. Find the most common rating for movies and TV shows
-4. Find the top 5 countries with the most content on Netflix
-5. Identify the longest movie
-6. Find content added in the last 5 years
-9. Count the number of content items in each genre
-10.Find each year and the average numbers of content release in India on netflix. 
-return top 5 year with highest avg content release!
-14. Find the top 10 actors who have appeared in the highest number of movies produced in India.
-15.
-Categorize the content based on the presence of the keywords 'kill' and 'violence' in 
-the description field. Label content containing these keywords as 'Bad' and all other 
-content as 'Good'. Count how many items fall into each category.
+-- 16 Business Problems & Solutions
+-- ===========================================
+-- NETFLIX SQL BUSINESS ANALYTICS PROJECT
+-- PostgreSQL
+-- 16 Business SQL Problems
+-- ===========================================
 
 
+ ===========================================
+ CATEGORY 1 : BASIC CONTENT ANALYTICS
+ ===========================================
+
+   
+ Q1. Count the number of Movies vs TV Shows
+
+ Q2. Find the most common rating for Movies and TV Shows
+
+ Q3. Find the Top 5 countries with the most content on Netflix
+
+ Q4. Identify the longest movie
+
+ Q5. Find content added in the last 5 years
 
 
+   
+ ===========================================
+ CATEGORY 2 : CONTENT EXPLORATION
+ ===========================================
+
+   
+ Q6. Count the number of content items in each genre
+
+ Q7. Find the Top 5 years with the highest average content release in India
+
+ Q8. Find the Top 10 actors who have appeared in the highest number of movies produced in India
+
+ Q9. Categorize content into 'Good' and 'Bad' based on the presence of the keywords
+     'kill' and 'violence' in the description
+
+   
+
+ ===========================================
+ CATEGORY 3 : BUSINESS CONTENT ANALYTICS
+ ===========================================
+
+
+   
+ Q10. Which countries contribute the highest percentage of Netflix total content library?
+
+ Q11. How has the Movies vs TV Shows ratio changed over the years?
+
+ Q12. Identify the Top 10 directors by total content and calculate their contribution to the platform
+
+ Q13. Which directors successfully created both Movies and TV Shows?
+
+
+
+   
+   
+ ===========================================
+ CATEGORY 4 : WINDOW FUNCTION ANALYTICS
+ ===========================================
+
+
+ Q14. Calculate the gap between consecutive releases for every director
+      to identify highly active creators
+
+ Q15. Identify the Top 3 newest titles in every country
+
+
+   
+ ===========================================
+ CATEGORY 5 : REGIONAL ANALYTICS
+ ===========================================
+
+ Q16. Which genre dominates each country?
+
+ Q17. Which content rating dominates every country?
 
 
 
